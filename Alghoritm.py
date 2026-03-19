236d78ba870f88d80850da51e6f087b22e078fb0
Sebastian1657 <miler.sebastian1101@gmail.com>
2026-03-19 20:28:58 +0100

    FINAL commit

--- Alghoritm.py
+++ Alghoritm.py
@@ -1,5 +1,6 @@
 import random
 import RandomSolution
+from DataConvertion import load_csv
 
 def CalculateFitnessScore(harmonia: dict, focus_stat: str) -> float:
     stats = ['pancerz', 'zdrowie', 'mana', 'obrazenia', 'charyzma']
@@ -8,8 +9,8 @@ def CalculateFitnessScore(harmonia: dict, focus_stat: str) -> float:
         raise ValueError(f"Nieznana statystyka: {focus_stat}. Dostępne: {stats}")
 
     # Definicja mnożników (można je później przenieść do konfiguracji globalnej)
-    WAGA_GLOWNA = 1.0
-    WAGA_POBOCZNA = 0.5
+    WAGA_GLOWNA = 0.5
+    WAGA_POBOCZNA = 0.6
     
     calkowity_wynik = 0.0
 
@@ -24,7 +25,7 @@ def CalculateFitnessScore(harmonia: dict, focus_stat: str) -> float:
     for item in harmonia['plecak']:
         ilosc = item.get('wylosowana_ilosc', 1)
         for statystyka in stats:
-            mnoznik = WAGA_GLOWNA if statystyka == focus_stat else WAGA_POBOCZNA
+            mnoznik = WAGA_GLOWNA if statystyka == focus_stat else WAGA_POBOCZNA/(len(stats)-1)
             # Mnożymy wartość statystyki przez ilość sztuk w stacku i przez wagę
             calkowity_wynik += (item.get(statystyka, 0) * ilosc) * mnoznik
             
@@ -36,12 +37,12 @@ def CalculateFitnessScore(harmonia: dict, focus_stat: str) -> float:
 def ImplementPenalty(harmonia: dict, max_weight: float, max_slots: int) -> None:
     if harmonia['calkowita_waga'] > max_weight:
         nadwaga = harmonia['calkowita_waga'] - max_weight
-        penalty = nadwaga * 2
+        penalty = nadwaga
         harmonia['fitness_score'] -= penalty
         
     if harmonia['zajete_sloty'] > max_slots:
         nadmiar_slotow = harmonia['zajete_sloty'] - max_slots
-        penalty = nadmiar_slotow * 5
+        penalty = nadmiar_slotow * 2
         harmonia['fitness_score'] -= penalty
 
 def BuildPlayerStats(harmonia: dict) -> dict:
@@ -72,19 +73,20 @@ def InitializeHarmonyMemory(HMS, focus_stat, max_weight=100.0, max_slots=20, ite
 def HarmonySearch(focus_stat, max_weight, max_slots, items_file="przedmioty.csv"):
     HMS = 25
     HMCR = 0.7
-    NI = 25000
-    r1 = random.randint(1,100)/100
-    HM = InitializeHarmonyMemory(HMS, focus_stat, max_weight, max_slots, items_file)
+    NI = 50000
+    items = load_csv(items_file)
+    HM = InitializeHarmonyMemory(HMS, focus_stat, max_weight, max_slots, items)
     NewSolution = {}
 
     for iteration in range(NI):
+        r1 = random.random()
         if r1 < HMCR:
             # losowanie nowego rozwiazania z losowych wartosci kolumn HM
             NewSolution = {}
             for key in HM[0].keys():
                 NewSolution[key] = HM[random.randint(0, HMS-1)][key]
         else:
-            NewSolution = HM[random.randint(0, HMS-1)]
+            NewSolution = RandomSolution.RandomSolution(items, max_slots)
         CalculateFitnessScore(NewSolution, focus_stat)
         ImplementPenalty(NewSolution, max_weight, max_slots)
         # porownanie NewSolution z najgorszym rozwiazaniem w HM
